3335f3f6204470ec52c7a3a8f7514abd150f4ab0
dipeshsingh253 <sinhdipesh@gmail.com>
2024-03-26 11:59:09 +0300

    basic recursion completed in python

--- Python/DSA/Basics/basic_recursion.py
+++ Python/DSA/Basics/basic_recursion.py
@@ -28,6 +28,37 @@ def sum_first_n_numbers(n):
         return 0
     return n + sum_first_n_numbers(n-1)
 
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+def reverse_array(arr,start,end):
+    if start >= end:
+        return
+
+    temp = arr[start]
+    arr[start] = arr[end]
+    arr[end] = temp
+
+    reverse_array(arr,start+1,end-1)
+
+def is_palindrome(string):
+    if len(string) <= 1:
+        return True
+    
+    if string[0] == string[-1]:
+        return is_palindrome(string[1:-1])
+    else:
+        return False
+    
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    return fibonacci(n-1) + fibonacci(n-2)
 
 
 print_something_ntimes(5)
@@ -35,3 +66,9 @@ print_name_ntimes("Charu",5)
 print_one_to_n(5)
 print_n_to_one(5)
 print(sum_first_n_numbers(5))
+print(factorial(5))
+arr = [1,2,3,4,5]
+reverse_array(arr,0,len(arr)-1)
+print(arr)
+print(is_palindrome("madam"))
+print(fibonacci(2))
