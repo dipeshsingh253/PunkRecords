bc3c6add1438051f0f7f2eabf24fd09afb7e610a
dipeshsingh253 <sinhdipesh@gmail.com>
2024-04-02 16:04:27 +0300

    planned for quicksort, binsearch, string but got work to do

---
+++ Java/DSA/Basics/BinarySearch.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class BinarySearch {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    static boolean binarySearch(int[] arr, int target){
+        boolean flag = false;
+
+
+        return flag;
+    }
+
+
+}
---
+++ Java/DSA/Strings/RemoveOuterMostParanthesis.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class RemoveOuterMostParanthesis {
+
+    public static void main(String[] args) {
+
+    }
+
+    
+
+}
