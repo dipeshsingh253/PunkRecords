c043a6bb416a44d6434dcc4bf049527ef84de3b6
dipeshsingh253 <sinhdipesh@gmail.com>
2024-04-03 09:08:31 +0300

    added quick sort, binary search and started working with strings

--- Java/DSA/Sorting/QuickSort.java
+++ Java/DSA/Sorting/QuickSort.java
@@ -1,6 +1,52 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
+    public static void main(String[] args) {
+
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        int n = arr.length;
+
+        quickSort(arr, 0, n - 1);
+
+        System.out.println("Sorted array:");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void quickSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int p = partition(arr,l,r);
+
+            quickSort(arr,l,p-1);
+            quickSort(arr,p+1,r);
+        }
+    }
+
+//    https://www.hackerearth.com/practice/algorithms/sorting/quick-sort/tutorial/
+    private static int partition(int[] arr, int l, int r) {
+
+        // here the partition will take such in place that all the elements before pivot will be less than it and after pivot will be greater than or equal to it.
+
+        int piv = arr[r];
+        int start = l-1;
+
+        for(int j=l;j<r;j++){
+            if(arr[j] < piv){
+                start++;
+
+                int temp = arr[j];
+                arr[j] = arr[start];
+                arr[start] = temp;
+            }
+        }
+
+        int temp = arr[r];
+        arr[r] = arr[start+1];
+        arr[start+1] = temp;
+
+        return start+1;
+    }
 
 }
--- Java/DSA/Strings/RemoveOuterMostParanthesis.java
+++ Java/DSA/Strings/RemoveOuterMostParanthesis.java
@@ -1,11 +1,61 @@
 package Strings;
 
+import java.util.Stack;
+
 public class RemoveOuterMostParanthesis {
 
     public static void main(String[] args) {
 
+        String s = "(()(()))";
+
+        System.out.println(removeOuterMostParanthesis(s));
+
+    }
+    static String removeOuterMostParanthesis(String s){
+
+        Stack<Character> stack = new Stack<>();
+        StringBuilder response = new StringBuilder();
+
+        for(char c:s.toCharArray()){
+           if(c == '('){
+               if(stack.size() > 0){
+                   response.append(c);
+               }
+               stack.push(c);
+           }else{
+               stack.pop();
+               if (stack.size() > 0){
+                   response.append(c);
+               }
+           }
+        }
+
+        return response.toString();
+
+
     }
 
-    
+    // This is a way to do it without using stack in may interviews they might ask such scenario
+    static String removeOuterWithoutStack(String s){
+
+        // if (len <= 2) return "";
+        StringBuilder newString = new StringBuilder();
+        int open = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                open++;
+                if (open > 1) newString.append('(');
+            }
+            else {
+                if (open > 1) newString.append(')');
+                open--;
+            }
+        }
+        return newString.toString();
+
+
+    }
+
 
 }
