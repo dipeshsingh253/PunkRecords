cad044eb180dfa6d094f1ad71bac9469110c0050
dipeshsingh253 <sinhdipesh@gmail.com>
2024-04-15 16:55:17 +0300

    django my project progress, removed old one due to some misconfigurations

--- Python/Django/myproject/myproject/boards/views.py
+++ Python/Django/myproject/myproject/boards/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.models import User
+from .models import Board, Post, Topic
+from .forms import NewTopicForm
 
+# Create your views here.
 def home(request):
-    return HttpResponse('Hello World !!!')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board,pk=pk)
+    return render(request, 'topics.html', {'board' : board})
+
+
+def new_topic(request,pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            print(topic.id)
+            return redirect('board_topics', pk=pk)
+    else:
+        form = NewTopicForm()
+
+    return render(request, 'new_topic.html', {'board':board, 'form':form})
