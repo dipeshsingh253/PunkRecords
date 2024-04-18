f07bb769d02094e773893a8dc962ca7e553a50d3
dipeshsingh253 <sinhdipesh@gmail.com>
2024-04-18 13:40:50 +0300

    django added  topicview and postrepy view

--- Python/Django/myproject/myproject/boards/forms.py
+++ Python/Django/myproject/myproject/boards/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Topic
+from .models import Topic, Post
 
 class NewTopicForm(forms.ModelForm):
     subject = forms.CharField(
@@ -18,3 +18,9 @@ class NewTopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['subject', 'message']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['message', ]
--- Python/Django/myproject/myproject/myproject/urls.py
+++ Python/Django/myproject/myproject/myproject/urls.py
@@ -47,5 +47,7 @@ urlpatterns = [
     path('settings/password/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
     name='password_change'),
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
-        name='password_change_done')
+        name='password_change_done'),
+    path('boards/<int:pk>/topics/<int:topic_pk>', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic')
 ]
