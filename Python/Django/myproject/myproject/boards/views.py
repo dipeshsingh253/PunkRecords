c3c98148a82cc10870104da0371dd9226d796af3
dipeshsingh253 <sinhdipesh@gmail.com>
2024-05-02 16:31:29 +0300

    pagination added for topics but not working on posts page

--- Python/Django/myproject/myproject/boards/views.py
+++ Python/Django/myproject/myproject/boards/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.views.generic import UpdateView
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from .models import Board, Post, Topic
 from .forms import NewTopicForm, PostForm
+from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def home(request):
@@ -13,9 +16,23 @@ def home(request):
 
 
 def board_topics(request, pk):
-    board = get_object_or_404(Board,pk=pk)
-    return render(request, 'topics.html', {'board' : board})
+    board = get_object_or_404(Board, pk=pk)
+    queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    page = request.GET.get('page', 1)
 
+    paginator = Paginator(queryset, 20)
+    print(page)
+    try:
+        topics = paginator.page(page)
+    except PageNotAnInteger:
+        # fallback to the first page
+        topics = paginator.page(1)
+    except EmptyPage:
+        # probably the user tried to add a page number
+        # in the url, so we fallback to the last page
+        topics = paginator.page(paginator.num_pages)
+
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 @login_required
 def new_topic(request,pk):
@@ -41,10 +58,25 @@ def new_topic(request,pk):
     return render(request, 'new_topic.html', {'board':board, 'form':form})
 
 
-def topic_posts(request, pk, topic_pk):
-    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-    return render(request, 'topic_posts.html', {'topic': topic})
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'topic_posts.html'
+    paginate_by = 20
 
+    def get_context_data(self, **kwargs):
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            # self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True
+        kwargs['topic'] = self.topic
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
+        queryset = self.topic.posts.order_by('created_at')
+        return queryset
 
 @login_required
 def reply_topic(request, pk, topic_pk):
@@ -62,6 +94,7 @@ def reply_topic(request, pk, topic_pk):
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
 
 
+@method_decorator(login_required, name='dispatch')
 class PostUpdateView(UpdateView):
     model = Post
     fields = ('message',)
@@ -69,6 +102,10 @@ class PostUpdateView(UpdateView):
     pk_url_kwarg = 'post_pk'
     context_object_name = 'post'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+    
     def form_valid(self,form):
         post = form.save(commit=False)
         post.updated_by = self.request.user
--- Python/Django/myproject/myproject/myproject/urls.py
+++ Python/Django/myproject/myproject/myproject/urls.py
@@ -48,7 +48,7 @@ urlpatterns = [
     name='password_change'),
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
         name='password_change_done'),
-    path('boards/<int:pk>/topics/<int:topic_pk>', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>', views.PostListView.as_view(), name='topic_posts'),
     path('boards/<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
     re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
         views.PostUpdateView.as_view(), name='edit_post')
